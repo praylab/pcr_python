71500434e65f4b551da17bea5e77a4c6af6bb9a5
Prayla Barli <praylabarli@gmail.com>
2025-11-24 13:15:27 +0100

    add a function to load struct in .mat file into pandas DataFrame

--- scripts/helper.py
+++ scripts/helper.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta
 
 import numpy as np
+import scipy.io 
+import pandas as pd
 
 def datenum_to_datetime_one(o, f, e=366) -> datetime:
     '''
@@ -29,3 +31,35 @@ def datenum_to_datetime(datenum: np.array) -> datetime:
         return datenum_to_datetime_one(ordinal, frac)
     
     return np.array([datenum_to_datetime_one(o, f) for o, f in zip(ordinal, frac)])
+
+
+def load_mat_to_df(file_path: str, struct_name: str) -> pd.DataFrame:
+    '''
+    Load a MATLAB struct array from a .mat file into a Pandas DataFrame.
+    :param file_path: str, path to the .mat file 
+    :param struct_name: str, name of the struct in the .mat file
+    :return: a DataFrame containing the struct data
+    '''
+
+    # load the .mat file 
+    mat_data = scipy.io.loadmat(file_path)
+
+    # check if struct_name exist
+    if struct_name not in mat_data:
+        raise ValueError(f"Struct '{struct_name}' not found in the .mat file")
+    
+    # Extract the struct data 
+    struct_data = mat_data[struct_name]
+
+    # Initialize an empty dictionary to store the struct data 
+    struct_dict = {}
+
+    # loop through each field in the struct and add it to the dictionary 
+    for field in struct_data[0].dtype.names:
+        struct_dict[field] = [entry[field][0][0] for entry in struct_data[0]]
+
+    # Return to a DataFrame
+    return pd.DataFrame(struct_dict)
+
+def date_add_day(date_ref: datetime, days: np.array) -> list:
+    return [date_ref + timedelta(days=day) for day in days]
