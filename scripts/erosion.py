7beb006531226bf71421c99634c3a00dc1d7dc0d
Prayla Barli <praylabarli@gmail.com>
2025-11-24 13:16:14 +0100

    new script erosion consist of erosion formulation functions

---
+++ scripts/erosion.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+# TODO: docstring
+def mendoza(storms_sample: pd.DataFrame) -> list:
+    '''
+    calculate erosion adapting mendoza and jimenez (2006) erosion model
+
+    '''
+
+    # constants 
+    Doe = 2.5 
+    g = 9.81
+    ws = 0.04 # 0.03 for 0.2 mm; 0.05 for 0.3 mm; 0.07 for 0.4 mm; 0.09 for 0.5 mm
+    m = 12/500 # average beach profile slope -> from where? 
+    d = 6
+
+    C1 = 2.069 #1.883;
+    C2 = 0.830 #0.842; 
+
+    hs = storms_sample['hs'].values
+    tp = storms_sample['tp'].values 
+    dur = storms_sample['duration'].values 
+
+    D = hs / (ws * tp)
+    JA = m * np.abs(Doe-D)**0.5
+
+    delV = C1*JA*dur + C2
+    delX = delV / d
+
+    return [delV, delX]
