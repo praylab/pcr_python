5f7c17fb20dc83aff0089ef4ce0bd1ac655f631e
praylab <praylabarli@gmail.com>
2026-07-28 17:42:36 +0200

    applying assumption of whole year simulation

--- pcr_simulation.py
+++ pcr_simulation.py
@@ -8,16 +8,17 @@ load_dotenv()
 
 print('Initialize the simulation ...')
 # initialize simulation length and number of sim
-date_start = np.datetime64('2000-01-01T00:00:00')
-date_end = np.datetime64('2100-12-31T23:00:00')
+year_start = '2020'
+year_end = '2120'
 
 model = PCRModel(
-    date_start=date_start,
-    date_end=date_end,
-    nr_simulation=1000,
-    nr_batch=100,
+    year_start=year_start,
+    year_end=year_end,
+    nr_simulation=100000,
+    nr_batch=1000,
     # SLR 
-    scenario='0',
+    scenario='ssp585',
+    slr_data_path='data/AR6_slr',
     wl0=0.0,
     lon_sl=82,
     lat_sl=7.5,
@@ -25,7 +26,7 @@ model = PCRModel(
     doe=2.5,
     ws=0.04,  # settling velocity, 0.03 for 0.2 mm; 0.05 for 0.3 mm; 0.07 for 0.4 mm; 0.09 for 0.5 mm
     d=2 + 1,  # dune height + depth of closure
-    rec_rate=7 / 365,
+    # rec_rate=7 / 365,
     m=0.024,
     c1=1.339,
     c2=1.983,
@@ -36,7 +37,7 @@ model = PCRModel(
     # wave data, TODO: from beach transect -> choose ERA5 point
     lon=82,
     lat=7.5,
-    wave_data_path='./data/ERA5/B3_offshore.nc',
+    wave_data_path='data/ERA5/B3_offshore.nc',
     data_mapper={'hs': 'swh', 'dir': 'mwd', 'tp': 'mwp', 'time': 'time'},
 )
 
